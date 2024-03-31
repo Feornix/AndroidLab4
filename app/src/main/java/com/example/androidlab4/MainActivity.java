88245cf85373311e36b41018860e57e925eff2c9
griml <grimlock.pheornix@bk.ru>
2024-03-31 21:20:50 +0600

    Передача даты в MainActivity

--- app/src/main/java/com/example/androidlab4/MainActivity.java
+++ app/src/main/java/com/example/androidlab4/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.androidlab4;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +14,21 @@ import com.example.androidlab4.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    if (data != null) {
+                        int selectedYear = data.getIntExtra("selectedYear", 0);
+                        int selectedMonth = data.getIntExtra("selectedMonth", 0)+1;
+                        int selectedDay = data.getIntExtra("selectedDay", 0);
+
+                        binding.textView4.setText(String.valueOf(selectedYear));
+                        binding.textView3.setText(String.valueOf(selectedMonth));
+                        binding.textView2.setText(String.valueOf(selectedDay));
+                    }
+                }
+            });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +40,12 @@ public class MainActivity extends AppCompatActivity {
                 String userInput = binding.editTextText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 intent.putExtra("userInput", userInput);
-                startActivity(intent);
+                Intent resintent = new Intent(MainActivity.this, SecondActivity.class);
+                activityResultLauncher.launch(intent);
             }
         });
         View view = binding.getRoot();
         setContentView(view);
     }
+
 }
--- app/src/main/java/com/example/androidlab4/SecondActivity.java
+++ app/src/main/java/com/example/androidlab4/SecondActivity.java
@@ -2,7 +2,9 @@ package com.example.androidlab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
@@ -15,6 +17,7 @@ import java.util.Calendar;
 public class SecondActivity extends AppCompatActivity {
     ActivitySecondBinding binding;
     DatePickerDialog datePickerDialog;
+    Intent resultIntent = new Intent();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +25,12 @@ public class SecondActivity extends AppCompatActivity {
         binding = ActivitySecondBinding.inflate(getLayoutInflater());
         String userInput = getIntent().getStringExtra("userInput");
         showDatePicker();
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
         binding.textView.setText(userInput);
         View view = binding.getRoot();
         setContentView(view);
@@ -35,8 +44,11 @@ public class SecondActivity extends AppCompatActivity {
 
         datePickerDialog = new DatePickerDialog(SecondActivity.this, new DatePickerDialog.OnDateSetListener() {
             @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                // Обработка выбранной даты
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                resultIntent.putExtra("selectedYear", year);
+                resultIntent.putExtra("selectedMonth", month);
+                resultIntent.putExtra("selectedDay", day);
+                setResult(Activity.RESULT_OK, resultIntent);
             }
         }, year, month, day);
         datePickerDialog.show();
